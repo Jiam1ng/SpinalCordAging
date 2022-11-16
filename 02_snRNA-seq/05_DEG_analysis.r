9126aad3c3832949fe38de89dd89aa09ed29147a
Jiam1ng <49811480+Jiam1ng@users.noreply.github.com>
2022-11-16 19:06:26 +0800

    Update and rename 04_DEG_analysis.r to 05_DEG_analysis.r

--- 02_snRNA-seq/05_DEG_analysis.r
+++ 02_snRNA-seq/05_DEG_analysis.r
@@ -3,13 +3,14 @@ library(ggplot2)
 library(ggpubr)
 library(stringr)
 
-sc.int = readRDS('/data5/lijiaming/projects/01_single-cell/01_SC/04_Seurat_final/SC_int_seu_obj.rds')
+sc.int = readRDS('SC_int_seu_obj.rds')
 celltypes = c("OPC", "OL", "Schwann", "Fib.Ast", "Pro.Ast", 
-              "Microglia", "Ependymal", "Pericyte","EC", 
+              "Microglia", 'Macrophage', 'T_cells', 'ImmunOL', 
+              "Ependymal", "Pericyte","EC", 
               'MN', "CNTN4.IN", "RBFOX1.IN")
 
 deg = c()
-Idents(sc.int) = paste(sc.int$celltype2, sc.int$age, sep = '_')
+Idents(sc.int) = paste(sc.int$celltype_IC, sc.int$age, sep = '_')
 for (cell in celltypes)
   try({
     tmp = FindMarkers(sc.int, ident.1 = paste0(cell, '_O'), ident.2 = paste0(cell, '_Y'))
@@ -18,6 +19,6 @@ for (cell in celltypes)
     deg = rbind(deg, tmp)
   })
 
-deg.sub = subset(deg, p_val < 0.05)
+deg.sub = subset(deg, p_val_adj < 0.05)
 
-write.csv('SC_snRNAseq_DEG.csv', row.names=F)
+write.csv(deg.sub, 'SC_snRNAseq_DEG_with_immune_cell.csv', row.names=F)
